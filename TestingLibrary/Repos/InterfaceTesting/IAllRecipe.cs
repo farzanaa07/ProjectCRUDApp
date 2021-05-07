76f5298ce62017d5d2bdaafad8af9d9a65935225
farzana-akter1 <farzana.akter@avanade.com>
2021-05-07 15:50:10 +0100

    New Changes

---
+++ TestingLibrary/Repos/InterfaceTesting/IAllRecipe.cs
@@ -0,0 +1,17 @@
+﻿using ProjectAppLibrary.Models;
+
+namespace TestingLibrary.Testing.InterfaceTesting
+{
+    public interface IAllRecipe
+    {
+        public int ID { get; set; }
+        public string RecipeName { get; set; }
+        public string PictureURL { get; set; }
+        public string Ingredients { get; set; }
+        public string Method { get; set; }
+        public int Servings { get; set; }
+        public LevelofDifficulty LevelofDifficulty { get; set; }
+    }
+
+
+}
